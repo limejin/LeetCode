cadcf54dcbe92a5da9273e457aba4a746da6853d
limejin <91420946+limejin@users.noreply.github.com>
2024-01-12 20:54:02 +0900

    Time: 2 ms (88.52%), Space: 47.2 MB (13.03%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -10,7 +10,7 @@ class Solution {
 
             return answer;
         }
-
+/*
         for(int i=0;i<numbers.length-1;i++){
             answer[0]=i+1;
             for(int j=i+1;j<numbers.length;j++){
@@ -22,7 +22,27 @@ class Solution {
             if(answer[1]!=0) break;
             else continue;
         }
-        
+*/
+
+        int start=0;
+        int end=numbers.length-1;
+
+        while(end>start){
+            int sum=numbers[start]+numbers[end];
+
+            if(sum==target){
+                answer[0]=start+1;
+                answer[1]=end+1;
+                break;
+            }else{
+                if(target>sum){
+                    start++;
+                }else{
+                    end--;
+                }
+            }
+        }
+
         return answer;
     }
 }
